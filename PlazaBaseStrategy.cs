1b5d3643efe0b79dc772f470ace30133d76f1d22
NikoN84p <123304934+NikoN84p@users.noreply.github.com>
2023-06-14 17:46:11 +0300

    Add files via upload

---
+++ PlazaBaseStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using StockSharp.BusinessEntities;
+// Для работы с коллекциямии
+// Для LINQ запросов
+
+// Для отправки почты
+// Для работы с файлами
+
+namespace PrismaBoy
+{
+    abstract class PlazaBaseStrategy: MyBaseStrategy
+    {
+        /// <summary>
+        /// Конструктор класса PlazaBaseStrategy
+        /// </summary>
+        protected PlazaBaseStrategy(List<Security> securityList, Dictionary<string, decimal> securityVolumeDictionary, TimeSpan timeFrame, decimal stopLossPercent, decimal takeProfitPercent)
+            : base(securityList, securityVolumeDictionary, timeFrame, stopLossPercent, takeProfitPercent)
+        {            
+            
+        }
+    }
+
+
+}
